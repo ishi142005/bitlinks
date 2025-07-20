fa97c2c2198a262c02556dcd7048e17ba0f0b953
Ishita Kanaujia <2k22.aiml.2212538@gmail.com>
2025-07-20 21:43:09 +0530

    Fixed the error

--- middleware.js
+++ middleware.js
@@ -1,6 +1,5 @@
 import { NextResponse } from 'next/server';
 import { getToken } from 'next-auth/jwt';
-import { initIndexes } from '@/lib/initIndexes'; 
 
 export async function middleware(req) {
 
